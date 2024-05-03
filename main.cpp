8590d8e255b218201379f3fc2f3e01bb43d0c2da
sultanofficial717 <145356504+sultanofficial717@users.noreply.github.com>
2024-05-03 10:43:16 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
 int main()
+class users
+{ public:
+int age;
+string name;
+float height;
+float weight;
+
+void getuserinfo(int n,string m, float z)
+{
+	n=age;
+	m=name;
+	z=height;
+}
+double calculateBMI(double weight, double height) {
+    // BMI formula: weight (kg) / (height (m) * height (m))
+    return weight / (height * height);
+}
+};
 {
 	return 0;
 }
